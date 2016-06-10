fcb830a6c11985e897831831f4cd77a0996584de
bryn2008 <brynla.lloyd@gmail.com>
2016-06-10 16:00:37 +0100

    added a couple more assert examples

--- src/test/java/com/farenda/junit/AssertionsTest.java
+++ src/test/java/com/farenda/junit/AssertionsTest.java
@@ -2,6 +2,7 @@ package com.farenda.junit;
 
 import static org.junit.Assert.*;
 
+import static java.util.Collections.emptyList;
 import org.junit.Test;
 
 public class AssertionsTest {
@@ -16,14 +17,39 @@ public class AssertionsTest {
 
     @Test
     public void shouldAssertArrayEquality() {
-        assertArrayEquals(new int[]{1, 2, 3}, new int[]{1,2,3});
-        assertArrayEquals("Those tables!", new boolean[]{true}, new boolean[]{true});
+        assertArrayEquals(new int[] { 1, 2, 3 }, new int[] { 1, 2, 3 });
+        assertArrayEquals("Those tables!", new boolean[] { true }, new boolean[] { true });
+    }
+
+    @Test
+    public void shouldShowInequality() {
+        assertNotEquals(3, 2 + 2);
+        assertNotEquals("Close, but no.", 3.14, 3.1415, 0.0001);
+    }
+
+    @Test
+    public void shouldAssertBooleanResult() {
+        assertTrue(emptyList().isEmpty());
+        assertTrue("It should be empty", emptyList().isEmpty());
+        
+        assertFalse("Yoda".isEmpty());
+        assertFalse("There shall only be one!", "Yoda".isEmpty());
     }
     
     @Test
-    public void shouldShowInequality(){
-        assertNotEquals(3,2+2);
-        assertNotEquals("Close, but no.", 3.14, 3.1415, 0.0001);
+    public void shouldTestNullness(){
+        assertNull(null);
+        assertNull("This one will have to be null", null);
+        
+        assertNotNull("Han Solo");
+        assertNotNull("Should not happen!", new Object());
+    }
+    
+    @Test
+    public void shouldFindTheSameObject(){
+        String jedi = "Luke S";
+        assertSame("There is only one Jedi!", jedi, jedi);
+        assertNotSame("These guys are diffrent!", new Object(), new Object());
     }
 
 }
